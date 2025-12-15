d1132619b127028c7b102ccd17f556e9926fbbeb
terraformhandsonlabs <terraformhandsonlabs@gmail.com>
2025-12-15 20:43:35 +0530

    Add files via upload

---
+++ vpc.tf
@@ -0,0 +1,23 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  tags = { Name = "two-tier-vpc" }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_subnet" "public" {
+  count                   = 2
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.${count.index}.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "ap-south-1a"
+}
+
+resource "aws_subnet" "private" {
+  count             = 2
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.${count.index + 10}.0/24"
+  availability_zone = "ap-south-1b"
+}
