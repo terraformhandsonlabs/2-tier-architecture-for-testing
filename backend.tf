3f2906411011ddb46f640ab754d9e1bac4cefa79
terraformhandsonlabs <terraformhandsonlabs@gmail.com>
2025-12-15 21:17:42 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "REPLACE_ME_TF_STATE_BUCKET"
-    key            = "2tier/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
-  }
-}
